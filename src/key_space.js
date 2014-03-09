ba7a0ab3bc84426e03746086342e597abd77e5ce
Mihails Butorins <mihails.butorins@gmail.com>
2014-03-10 00:30:22 +0200

    Initial demo page

--- src/key_space.js
+++
@@ -1,9 +0,0 @@
-function KeySpace() {
-  Key.call(this, arguments);
-
-  this.id = "mlkeyboard-space";
-  this.default_value = ' ';
-}
-
-KeySpace.prototype = new Key();
-KeySpace.prototype.constructor = KeySpace;
