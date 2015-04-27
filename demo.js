9ff336b01f1f720c9d6b4ecf66be86272d8d0234
Mihails Butorins <mihails.butorins@gmail.com>
2015-04-27 12:17:36 +0200

    Updated Demo page

--- demo.js
+++ demo.js
@@ -1,3 +1,15 @@
 $(document).ready(function(){
-  $('input').mlKeyboard({layout: 'en_US'});
+  $('input#example-1').mlKeyboard({layout: 'en_US'});
+});
+
+$(document).ready(function(){
+  $('input#example-2').mlKeyboard({layout: 'ru_RU'});
+});
+
+$(document).ready(function(){
+  $('input#example-3').mlKeyboard({layout: 'es_ES'});
+});
+
+$(document).ready(function(){
+  $('input#example-4').mlKeyboard({layout: 'it_IT', trigger: '#example-4-btn'});
 });
