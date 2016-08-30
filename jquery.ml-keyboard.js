9db325d69e64661c04442c5fd251d426184a9aa8
Mario Perrotta <perrotta.mr@gmail.com>
2016-08-30 09:46:40 +0200

    Resolved TAB key issue

--- jquery.ml-keyboard.js
+++ jquery.ml-keyboard.js
@@ -109,9 +109,10 @@ KeyTab.prototype = new Key();
 KeyTab.prototype.constructor = KeyTab;
 
 KeyTab.prototype.defaultClickAction = function() {
-  // TODO: It's doesn't work if inputs has different parents
-  this.keyboard.$current_input.next(":input").focus();
+  this.keyboard.hideKeyboard();
+  $(":input").eq($(":input").index(this.keyboard.$current_input)+1).focus();
 };
+
   function KeyCapsLock() {
   Key.call(this, arguments);
 
