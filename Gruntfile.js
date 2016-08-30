9c22fac82110808683afec2ccc36559194c0f5cc
Mario Perrotta <perrotta.mr@gmail.com>
2016-08-30 09:44:36 +0200

    Added default task

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,4 +45,5 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-include-replace');
 
   grunt.registerTask('build', ['includereplace', 'sass', 'uglify']);
+  grunt.registerTask('default', ['includereplace', 'sass', 'uglify']);
 };
