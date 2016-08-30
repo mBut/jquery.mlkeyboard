91503a16cb23321811f59adbdd23ea97ba4b03b6
Mario Perrotta <perrotta.mr@gmail.com>
2016-08-30 09:45:26 +0200

    Now TAB key go to next input field

--- src/key_tab.js
+++ src/key_tab.js
@@ -9,6 +9,6 @@ KeyTab.prototype = new Key();
 KeyTab.prototype.constructor = KeyTab;
 
 KeyTab.prototype.defaultClickAction = function() {
-  // TODO: It's doesn't work if inputs has different parents
-  this.keyboard.$current_input.next(":input").focus();
+  this.keyboard.hideKeyboard();
+  $(":input").eq($(":input").index(this.keyboard.$current_input)+1).focus();
 };
