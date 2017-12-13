411f67482fdf606e1a3cce17d47e0ff496bd5066
David Brandl <db@davidb.at>
2017-12-13 13:08:10 +0100

    Add files via upload
    Added German keyboard layout.

--- jquery.ml-keyboard.js
+++ jquery.ml-keyboard.js
@@ -440,6 +440,65 @@ Keyboard.prototype.changeKeysState = function() {
 
 var mlKeyboard = mlKeyboard || {layouts: {}};
 
+mlKeyboard.layouts.de_DE = [
+  {d: '`', u: '~'},
+  {d: '1',u: '!'},
+  {d: '2',u: '"'},
+  {d: '3',u: '§'},
+  {d: '4',u: '$'},
+  {d: '5',u: '%'},
+  {d: '6',u: '&'},
+  {d: '7',u: '/'},
+  {d: '8',u: '('},
+  {d: '9',u: ')'},
+  {d: '0',u: '='},
+  {d: '-',u: '_'},
+  {d: 'ß',u: '?'},
+  {}, // Delete,
+  {}, // Tab,
+  {d: 'q',u: 'Q'},
+  {d: 'w',u: 'W'},
+  {d: 'e',u: 'E'},
+  {d: 'r',u: 'R'},
+  {d: 't',u: 'T'},
+  {d: 'z',u: 'Z'},
+  {d: 'u',u: 'U'},
+  {d: 'i',u: 'I'},
+  {d: 'o',u: 'O'},
+  {d: 'p',u: 'P'},
+  {d: 'ü',u: 'Ü'},
+  {d: '+',u: '*'},
+  {d: '+',u: '*'},
+  {}, // Caps lock
+  {d: 'a',u: 'A'},
+  {d: 's',u: 'S'},
+  {d: 'd',u: 'D'},
+  {d: 'f',u: 'F'},
+  {d: 'g',u: 'G'},
+  {d: 'h',u: 'H'},
+  {d: 'j',u: 'J'},
+  {d: 'k',u: 'K'},
+  {d: 'l',u: 'L'},
+  {d: 'ö',u: 'Ö'},
+  {d: 'ä',u: 'Ä'},
+  {}, // Return
+  {}, // Left shift
+  {d: '<',u: '>'},
+  {d: 'y',u: 'Y'},
+  {d: 'x',u: 'X'},
+  {d: 'c',u: 'C'},
+  {d: 'v',u: 'V'},
+  {d: 'b',u: 'B'},
+  {d: 'n',u: 'N'},
+  {d: 'm',u: 'M'},
+  {d: ',',u: ';'},
+  {d: '.',u: ':'},
+  {}, // Right shift
+  {}  // Space
+];
+
+var mlKeyboard = mlKeyboard || {layouts: {}};
+
 mlKeyboard.layouts.en_US = [
   {d: '`', u: '~'},
   {d: '1',u: '!'},
